2f7f7d2182534e11e2573764e1c0b6437074ffda
Julio <juliocesarvanideoliveira@gmail.com>
2026-06-12 21:34:17 -0300

    Added assets

--- game_code/Menu.py
+++ game_code/Menu.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load("./asset/menuScreen.png")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
         pass
